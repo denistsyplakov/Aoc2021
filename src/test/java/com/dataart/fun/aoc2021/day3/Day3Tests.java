ec0cef3893bc711e4a75ceb291013b83d3746d86
Denis Tsyplakov <denis.tsyplakov@dataart.com>
2021-12-03 16:11:05 +0300

    day3 tests

--- src/test/java/com/dataart/fun/aoc2021/day3/Day3Tests.java
+++ src/test/java/com/dataart/fun/aoc2021/day3/Day3Tests.java
@@ -15,6 +15,9 @@ class Day3Tests {
 	@Autowired
 	Day3Task1 day3Task1;
 
+	@Autowired
+	Day3Task2 day3Task2;
+
 	@Value("classpath:day3/task-input-test.txt")
 	Resource testInput;
 
@@ -22,6 +25,12 @@ class Day3Tests {
 	void testDay3Task1() throws IOException {
 		day3Task1.taskInput = testInput;
 		Assert.isTrue( day3Task1.solve() == 198,"day3Task1.solve() == 198");
+	}
+
+	@Test
+	void testDay3Task2() throws IOException {
+		day3Task2.taskInput = testInput;
+		Assert.isTrue( day3Task2.solve() == 230,"day3Task2.solve() == 230");
 	}
 
 }
